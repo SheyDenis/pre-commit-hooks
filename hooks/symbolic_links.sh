bc7b2843a12212337c543982aedd6ee0a0647b4c
SheyDenis <denis.sheyer@gmail.com>
2023-02-22 23:42:28 +0200

    Add symbolic link hook.

--- hooks/symbolic_links.sh
+++ hooks/symbolic_links.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-# TODO
+source "$(dirname ${0})/utils.sh"
 
-exit 1
+echo "@: $@"
+rc=0
+for staged_file in ${@}; do
+    echo_debug "Handling file [${staged_file}]"
+    if [[ -L "${staged_file}" ]]; then
+        rc=1
+        echo_error "File [${staged_file}] is a symbolic link to [$(file ${staged_file} | grep -Eo 'symbolic link to .*' | sed 's/symbolic link to //')]"
+    fi
+done
+
+echo_info "rc [${rc}]"
+exit ${rc}
