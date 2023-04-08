d3a618e450b35ad880007a8607b05417a6cb094d
Denis Sh <SheyDenis@users.noreply.github.com>
2023-04-08 17:10:41 +0300

    Added dry run switches.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Git hooks for personal use based on https://github.com/pre-commit/pre-commit .
 - [ ] Rewrite `setup.py` as `setup.cfg`.
 - [ ] Cleanup `setup.py`.
 - [ ] Add line numbers to dev-marker hook.
+- [ ] Implement `cppcheck` hook.
 - [ ] Implement `utilities.proc.wait_to_finish()` waiting and TO kill.
-- [ ] Add `--dry-run` / `--no-dry-run` to `utilities.argparse.get_base_parser()`.
 - [ ] Cleanup log messages.
 - [ ] Log error messages if hook failed to execute.
--- personal_pre_commit_hooks/utilities/argparse.py
+++ personal_pre_commit_hooks/utilities/argparse.py
@@ -3,12 +3,15 @@ from argparse import ArgumentParser
 from personal_pre_commit_hooks.utilities.constants import LOGGER_DEBUG_ARG
 
 
-def get_base_parser() -> ArgumentParser:
+def get_base_parser(**kwargs) -> ArgumentParser:
     """Get the base parser with the common arguments."""
-    # TODO - Add `--dry-run` / `--no-dry-run`
-    parser = ArgumentParser()
+    parser = ArgumentParser(**kwargs)
 
     parser.add_argument('filenames', nargs='*', help='Filenames to check')
     parser.add_argument(LOGGER_DEBUG_ARG, action='store_true', help='Set logger to debug level')
+
+    dry_run_group = parser.add_mutually_exclusive_group()
+    dry_run_group.add_argument('--dry-run', action='store_true', dest='dry_run', default=True)
+    dry_run_group.add_argument('--no-dry-run', action='store_false', dest='dry_run')
 
     return parser
